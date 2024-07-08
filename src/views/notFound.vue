2955645e55bec8131e98bb4cba7400d82734600f
gyupp <kkb75281@gmail.com>
2024-07-08 16:49:33 +0900

    404 page

---
+++ src/views/notFound.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+br
+div(style="text-align:center")
+    router-link(to="/")
+            img(src="@/assets/img/logo/symbol-logo.png" style="width: 40px;")
+    
+    br
+    br
+    br
+
+    div(style="font-weight:bold; font-size:2rem") 404
+    div Page not found
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
