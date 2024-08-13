b7a7e72c1ed41564479c191758c0051164ddfd22
gyupp <kkb75281@gmail.com>
2024-08-14 08:50:19 +0900

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
   plugins: [
+    visualizer({
+      open: false, // 자동으로 브라우저를 열지 않음
+      filename: 'dist/manbalboy-bundle-report.html', // 리포트 이름과 경로
+      template: 'treemap', // treemap, sunburst, network, etc.
+    }),
     vue(),
   ],
   resolve: {
